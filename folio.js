904c609d82b0c7d51c119ac646f4bc9e1705d17d
dedeherman <63778560+dedeherman@users.noreply.github.com>
2020-08-17 04:30:38 +0700

    Update folio.js

--- folio.js
+++ folio.js
@@ -1,41 +1,51 @@
 //smoothScroll
 
 var posY = 0;
-var jarak = 15;
+var jarak = 5;
 
-function smoothScroll(id){
-  var target = document.getElementById(id).offsetTop; // jarak antara atas dan div
+function smoothScroll(id) {
+    var target = document.getElementById(id).offsetTop; // jarak antara atas dan div
 
-  var scrollAnimate = setTimeout(function(){
-    smoothScroll(id);
-  }, 5) // fungsi, waktu
+    var scrollAnimate = setTimeout(function() {
+            smoothScroll(id);
+        }, 10) // fungsi, waktu
 
-  posY = posY + jarak;
+    posY = posY + jarak;
 
-  //berhenti  pada bag tertentu
-  if(posY >= target){
-    clearTimeout(scrollAnimate);
-    posY = 0;
-  }else{
-    window.scroll(0, posY) //x dan y
-  }
+    //berhenti  pada bag tertentu
+    if (posY >= target) {
+        clearTimeout(scrollAnimate);
+        posY = 0;
+    } else {
+        window.scroll(0, posY) //x dan y
+    }
 
 
 
-  return false;
+    return false;
 }
 
 //fungsi untuk validasi formular
 
-function validasi(form){
-  var lolos = true;
+function validasi(form) {
+    var lolos = true;
 
-  for(i=0; i<3; i++){
-    if(form[i].value.length <= 0){
-      form[i].style.borderColor = 'red';
-      form[i].insertAdjacentHTML('afterend', "<div class='error'> tidak boleh kosong </div>" );
+    for (i = 0; i < 3; i++) {
+        if (form[i].value.trim().length <= 0) {
 
-      return false;
-    }
-  } // end for
+            // kalau belum ada error        
+            if (form[i].nextElementSibling.className != 'error') {
+                form[i].style.borderColor = 'red';
+                form[i].insertAdjacentHTML('afterend', "<div class='error'> tidak boleh kosong </div>");
+            }
+
+            lolos = false;
+        } else {
+            if (form[i].nextElementSibling.className == 'error') {
+                form[i].style.borderColor = '#0074b9';
+                form[i].nextElementSibling.remove();
+            }
+        }
+    } // end for
+    return lolos;
 }
